d1dedeae9e19bd811a8d8f7d28d57666d67b5d22
BalaGit155 <balamurugancr2005@gmail.com>
2025-07-07 14:47:49 +0530

    tested the api and added screenshots

--- backend_test_submission/main.py
+++ backend_test_submission/main.py
@@ -7,6 +7,7 @@ from pydantic import BaseModel
 from datetime import datetime, timedelta
 from typing import Optional
 from logging_middleware import logger
+from fastapi.responses import RedirectResponse
 
 
 
@@ -34,6 +35,7 @@ def create_short_url(data:urlRequest):
         "url": data.url,
         "expiry": expiry,
         "created": datetime.utcnow(),
+        "clicks":[]
     }
 
     logger.log("backend", "info", "controller", f"short code created: {code} for url: {data.url}")
@@ -41,4 +43,41 @@ def create_short_url(data:urlRequest):
     return {
         "shortlink":f"http://localhost:8000/{code}",
         "expiry": expiry.isoformat(),
+    }
+
+# to redirect to original url
+@app.get("/{shortcode}")
+def redirect(shortcode:str, request: Request):
+    if shortcode not in db:
+        logger.log("backend","error","db",f"{shortcode} not found")
+        raise HTTPException(status_code=404, detail="Shortcode not found")
+    
+    if datetime.utcnow() > db[shortcode]["expiry"]:
+        logger.log("backend","error","db",f"{shortcode} expired")
+        raise HTTPException(status_code=410, detail="Shortcode expired")
+       
+    db[shortcode]["clicks"].append({
+        "timestamp": datetime.utcnow().isoformat(),
+        "referrer": request.headers.get("referer", "unknown"),
+        "location": "unkonwn"
+    })
+
+    RedirectResponse(url=db[shortcode]["url"])
+
+    return {"redirect": db[shortcode]["url"]}
+
+# to get statistics
+@app.get("/shorturls/{shortcode}")
+def stats(shortcode: str):
+    if shortcode not in db:
+        logger.log("backend", "error", "controller", f"Stats failed: {shortcode}")
+        raise HTTPException(status_code=404, detail="Shortcode not found")
+
+    entry = db[shortcode]
+    return {
+        "url": entry["url"],
+        "created": entry["created"],
+        "expiry": entry["expiry"],
+        "clicks": entry["clicks"],
+        "clickCount": len(entry["clicks"])
     }
